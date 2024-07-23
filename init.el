4cf7da66e9593d98884454d3f8ea1884b2128565
Rohit Patnaik <quanticle@quanticle.net>
2024-07-23 10:01:56 -0500

    Revert removing addl. promotion/demotion commands
    I lied. It turns out that I still use the non-arrow promotion and demotion
    commands all the time, and it's really annoying to have them gone.

--- init.el
+++ init.el
@@ -221,6 +221,10 @@ name to the default value specified by FRAME-TITLE-FORMAT."
 (use-package org
   :ensure t
   :bind (:map org-mode-map
+         ("C-c >" . org-metaright)
+         ("C-c <" . org-metaleft)
+         ("C-c v" . org-metadown)
+         ("C-c M-v" . org-metaup)
          ("C-c ," . org-insert-structure-template)
          ("<insert>" . org-insert-structure-template)
          ("C-c C-'" . org-edit-special)
